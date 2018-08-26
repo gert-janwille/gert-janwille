0f9dc9c1ac1295153a6499c83c6f4c36197c7cf3
Gert-Jan Wille <gertjanwille.w@gmail.com>
2018-08-26 12:44:59 +0200

    :art: new overview

--- src/js/components/projects/ProjectItem.jsx
+++ src/js/components/projects/ProjectItem.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {createValidString} from '../../lib/util';
 
-const ProjectItem = ({id, title, services, mainImage}) => {
+const ProjectItem = ({title, services, mainImage, color}) => {
 
   return (
-    <Link to={`/projects/${createValidString(title)}`} className="project-item">
+    <Link to={`/projects/${createValidString(title)}`} className="project-item" style={{background: `linear-gradient(${color[0]}, ${color[1]})`}}>
+      <img src={`/uploads/${createValidString(title)}/${mainImage}`} onError={({currentTarget}) => currentTarget.src = '/assets/img/default/main-image.png'}  width="100%" alt={title}/>
 
-      <div className="image-holder">
-
-        <img src={`/uploads/${createValidString(title)}/${mainImage}`} onError={({currentTarget}) => currentTarget.src = '/assets/img/default/main-image.png'}  width="100%" alt={title}/>
-        {/* <p className="views"><span className="eye small-icon"></span>100</p> */}
-
+      <div className="blur-overlay">
+        <h3 className="project-name">{title}</h3>
+        <h4 className="what-done">{services.join(' | ')}</h4>
       </div>
-
-      <h3 className="project-name">{title}</h3>
-      <h4 className="what-done">{services.join(' | ')}</h4>
 
     </Link>
   );
